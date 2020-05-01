b38ae518d1007ddcb4747ef8965029ccc7933c8f
Emmanuel Ivan <64347802+EmmanuelIvan@users.noreply.github.com>
2020-05-01 03:32:20 +0000

    Create Logical_operators.py

---
+++ Logical_operators.py
@@ -0,0 +1,16 @@
+#Veremos como se usan los operadores logicos y cual es su resultado
+
+#En el operado AND las dos condicones deben de ser positivas para que el resultado sea true
+print (5 > 2 and 9 < 11)
+
+#En caso de no cumplir nos devolvera false
+print (5 > 2 and 10 < 2)
+
+#En el operador OR las dos condiciones no deben de ser positivas para que el resultado sea true
+print (5 > 2 or 10 < 3)
+
+#En caso de no cumplir nos devolvera false
+print (5 > 19 or 10 < 3)
+
+#En el operador not sera el cambio si en este caso es true mostrara false y visebersa
+print (not (5 > 7) )
